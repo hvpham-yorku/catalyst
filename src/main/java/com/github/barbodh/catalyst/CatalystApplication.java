22167ca04c78d76e55d01f52ffb569d93f2fb794
BarbodH <habibi.k.barbod@gmail.com>
2024-11-04 23:12:38 -0500

    Initial commit

---
+++ src/main/java/com/github/barbodh/catalyst/CatalystApplication.java
@@ -0,0 +1,13 @@
+package com.github.barbodh.catalyst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalystApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalystApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/barbodh/catalyst/CatalystApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.barbodh.catalyst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalystApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
