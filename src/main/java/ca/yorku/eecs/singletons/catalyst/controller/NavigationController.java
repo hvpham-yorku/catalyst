c8a664721b367a3a5670776de158a28496bdddc5
Keven Nguyen <122302117+kevnngyen@users.noreply.github.com>
2024-11-06 18:05:36 -0500

    Added comment explaing what each function does in controller

--- src/main/java/ca/yorku/eecs/singletons/catalyst/controller/NavigationController.java
+++ src/main/java/ca/yorku/eecs/singletons/catalyst/controller/NavigationController.java
@@ -5,21 +5,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class NavigationController {
+
+    /* Locates any element with href with '/' and returns a string "index"
+       to spring for rendering (finding and opening) the index.html
+     */
     @GetMapping("/")
     public String index() {
         return "index";
     }
 
+    /* Locates any element with href with '/patients' and returns a string "patients"
+       to spring for rendering (finding and opening) the patients.html
+    */
     @GetMapping("/patients")
     public String patients() {
         return "patients";
     }
 
+    /* Locates any element with href with '/optometrists' and returns a string "optometrists"
+       to spring for rendering (finding and opening) the optometrists.html
+    */
     @GetMapping("/optometrists")
     public String optometrists() {
         return "optometrists";
     }
 
+    /* Locates any element with href with '/assessments' and returns a string "assessments"
+       to spring for rendering (finding and opening) the assessments.html
+    */
     @GetMapping("/assessments")
     public String assessments() {
         return "assessments";
